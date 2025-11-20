a84744a73090ead18a00679a327989d5043ad8fb
Bo <a@a.com>
2025-11-20 13:03:37 +0800

    .

--- reference/cuda/rwkv7_state_fwd_fp16.cu
+++ reference/cuda/rwkv7_state_fwd_fp16.cu
@@ -5,11 +5,9 @@
 
 typedef at::Half dtype;
 
-template <int N>
-struct SharedVec { float x[N+1]; };
+template <int N> struct SharedVec { float _[N+1]; };
 
-template <int N, typename F>
-__launch_bounds__(N, 2)
+template <int N, typename F> __launch_bounds__(N, 2)
 __global__ void kernel_forward(const int B, const int T, const int C, const int H,
                                float *__restrict__ _state, const F *__restrict__ const _r, const F *__restrict__ const _w, const F *__restrict__ const _k, const F *__restrict__ const _v,
                                const F *__restrict__ const _a, const F *__restrict__ const _b, F *__restrict__ const _y)
@@ -24,36 +22,36 @@ __global__ void kernel_forward(const int B, const int T, const int C, const int 
     for (int j = 0; j < N; ++j)
         state[j] = _state[j];
 
-    __shared__ SharedVec<N> sh_r;
-    __shared__ SharedVec<N> sh_w;
-    __shared__ SharedVec<N> sh_k;
-    __shared__ SharedVec<N> sh_a;
-    __shared__ SharedVec<N> sh_b;
+    __shared__ SharedVec<N> r;
+    __shared__ SharedVec<N> w;
+    __shared__ SharedVec<N> k;
+    __shared__ SharedVec<N> a;
+    __shared__ SharedVec<N> b;
 
     for (int _t = 0; _t < T; ++_t)
     {
         const int t = bbb*T*C + h*_N_ + i + _t * C;
         __syncthreads();
-        sh_r.x[i] = float(_r[t]);
-        sh_w.x[i] = __expf(-0.6065306597f * float(_w[t])); // 0.6065306597 = exp(-0.5)
-        sh_k.x[i] = float(_k[t]);
-        sh_a.x[i] = float(_a[t]);
-        sh_b.x[i] = float(_b[t]);
+        r._[i] = float(_r[t]);
+        w._[i] = __expf(-0.6065306597f * float(_w[t])); // 0.6065306597 = exp(-0.5)
+        k._[i] = float(_k[t]);
+        a._[i] = float(_a[t]);
+        b._[i] = float(_b[t]);
         __syncthreads();
 
         float sa = 0.0f;
         #pragma unroll
         for (int j = 0; j < N; ++j)
-            sa = fmaf(state[j], sh_a.x[j], sa);
+            sa = fmaf(state[j], a._[j], sa);
 
-        const float vv = float(_v[t]);
+        const float v_ = float(_v[t]);
         float y = 0.0f;
         #pragma unroll
         for (int j = 0; j < N; ++j)
         {
             float s = state[j];
-            s = fmaf(s, sh_w.x[j], fmaf(sa, sh_b.x[j], sh_k.x[j] * vv));
-            y = fmaf(s, sh_r.x[j], y);
+            s = fmaf(s, w._[j], fmaf(sa, b._[j], k._[j] * v_));
+            y = fmaf(s, r._[j], y);
             state[j] = s;
         }
         _y[t] = F(y);
